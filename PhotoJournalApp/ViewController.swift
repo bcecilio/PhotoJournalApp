983f34f0b5a03b9850d7ec37acb144b7172c3147
brendon cecilio <brendon.cecilio@gmail.com>
2020-01-23 10:17:54 -0500

    Initial Commit

---
+++ PhotoJournalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoJournalApp
+//
+//  Created by Brendon Cecilio on 1/23/20.
+//  Copyright © 2020 Brendon Cecilio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
